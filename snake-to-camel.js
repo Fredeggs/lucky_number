addb6f670fbbed08b8363bc8f13d3b74a944057d
fredeggs <ian.dauphin@gmail.com>
2022-08-10 20:26:20 -0400

    completed JS exercises

--- snake-to-camel.js
+++ snake-to-camel.js
@@ -1,2 +1,10 @@
-function snakeToCamel() {}
-
+function snakeToCamel(variable) {
+  let splitUnderscore = variable.split("_");
+  let camelArray = [splitUnderscore[0]];
+  for (let index = 1; index < splitUnderscore.length; index++) {
+    camelArray.push(
+      splitUnderscore[index][0].toUpperCase() + splitUnderscore[index].slice(1)
+    );
+  }
+  return camelArray.join("");
+}
--- star-out-grid.js
+++ star-out-grid.js
@@ -1,2 +1,23 @@
 function starOutGrid(grid) {
+  let columnsWithStars = [];
+  let rowsWithStars = [];
+  for (let i = 0; i < grid.length; i++) {
+    for (let k = 0; k < grid[i].length; k++) {
+      if (grid[i][k] === "*") {
+        rowsWithStars.push(i);
+        columnsWithStars.push(k);
+      }
+    }
+  }
+  for (const rowNum of rowsWithStars) {
+    grid[rowNum].fill("*");
+  }
+
+  for (const columnNum of columnsWithStars) {
+    for (const row of grid) {
+      row[columnNum] = "*";
+    }
+  }
+
+  return grid;
 }

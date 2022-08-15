3bd5ec511b5d73b009b320e3f46b55921b17f60d
fredeggs <ian.dauphin@gmail.com>
2022-08-14 23:27:50 -0400

    completed assignment

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+import requests
+from random import randrange
+from flask import Flask, render_template, request, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = False
+app.config["SECRET_KEY"] = "chickens123"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+debug = DebugToolbarExtension(app)
 
 
 @app.route("/")
@@ -8,3 +17,35 @@ def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+
+@app.route("/api/get-lucky-num", methods=["POST"])
+def get_lucky_num():
+    errors = {}
+    params = request.get_json()
+    name, email, color, year = (
+        params["name"].strip(),
+        params["email"].strip(),
+        params["color"].strip(),
+        params["year"],
+    )
+    if name == None:
+        errors["name"] = "This field is required."
+    if email == None:
+        errors["email"] = "This field is required."
+    if color not in ["red", "green", "orange", "blue"]:
+        errors["color"] = "Invalid value, must be one of: red, green, orange, blue."
+    if year < 1900 or year > 2000:
+        errors["year"] = "Invalid year. Value must be between 1900 and 2000"
+
+    if errors.keys():
+        return jsonify(errors=errors)
+    else:
+        res_num = requests.get(f"http://numbersapi.com/{randrange(1, 100)}/trivia?json")
+        num_data = res_num.json()
+        res_year = requests.get(f"http://numbersapi.com/{year}/year?json")
+        year_data = res_year.json()
+        return jsonify(
+            num={"fact": num_data["text"], "num": num_data["number"]},
+            year={"fact": year_data["text"], "year": year_data["number"]},
+        )
--- lucky-nums/static/lucky.js
+++ lucky-nums/static/lucky.js
@@ -1,12 +1,62 @@
 /** processForm: get data from form and make AJAX call to our API. */
 
-function processForm(evt) {
+async function processForm(evt) {
+  evt.preventDefault();
+  form_data = {
+    name: $("#name").val(),
+    email: $("#email").val(),
+    year: parseInt($("#year").val()),
+    color: $("#color").val(),
+  };
+  resp = await axios.post("/api/get-lucky-num", (json = form_data));
+
+  handleResponse(resp);
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
 function handleResponse(resp) {
+  if (resp.data.errors) {
+    if (resp.data.errors.name) {
+      $("#name-err").append(`${resp.data.errors.name}`);
+    }
+    if (resp.data.errors.email) {
+      $("#email-err").append(`${resp.data.errors.email}`);
+    }
+    if (resp.data.errors.year) {
+      $("#year-err").append(`${resp.data.errors.year}`);
+    }
+    if (resp.data.errors.color) {
+      $("#color-err").append(`${resp.data.errors.color}`);
+    }
+  } else {
+    $("#lucky-results").append(
+      `<p>Your lucky number is ${resp.data.num.num} ${resp.data.num.fact}</p>
+         <p>Your birth year ${resp.data.year.year} fact <b>is</b> ${resp.data.year.fact}</p>`
+    );
+  }
 }
 
-
 $("#lucky-form").on("submit", processForm);
+
+// $("form").on("click", ".create-cupcake", createCupcake);
+
+// async function createCupcake(e) {
+//   e.preventDefault();
+//   new_cupcake = {
+//     flavor: $(".flavor").val(),
+//     size: $(".size").val(),
+//     rating: $(".rating").val(),
+//     image: $(".image").val(),
+//   };
+//   resp = await axios.post("/api/cupcakes", (json = new_cupcake));
+//   $(".cupcake-list").children().remove();
+//   resp2 = await axios.get("/api/cupcakes");
+//   $(".cupcake-list").append("<ul></ul>");
+//   const CUPCAKES = resp2.data.cupcakes;
+//   for (const CUPCAKE of CUPCAKES) {
+//     $("ul").append(
+//       `<li><button class="show-details" data-id=${CUPCAKE.id}>${CUPCAKE.flavor}</button></li>`
+//     );
+//   }
+// }
